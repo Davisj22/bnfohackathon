f2833aff5ad810182422fb63ea85faa3be0f2935
yuriprym <samuel.young.103@gmail.com>
2016-04-23 02:44:01 -0400

    add the base format for the begin

---
+++ datavis.py
@@ -0,0 +1,17 @@
+file
+import math
+import matplotlib.pyplot as plt 
+
+class data:
+  def fata__Data(weight,headers,figures):
+   headers.read()
+
+
+
+
+
+
+
+class visual_histogram:
+ def vis_hist(,,,)
+  
